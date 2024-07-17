89a2443cd11859d828825702e378de1c05a5f383
Aryel Ramos <aryel.goncalves@livsaude.com.br>
2024-07-17 09:54:49 -0300

    chore: config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,8 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "rules": {
+      "quotes": ["error", "double", { "avoidEscape": true }]
+    }
   },
 };
