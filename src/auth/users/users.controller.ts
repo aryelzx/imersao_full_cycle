fb3eccb9832a51f0d7a02a61da8cd73402d4f579
aryelzx <arielcearamor102@gmail.com>
2024-07-21 10:17:10 -0300

    feat: adding presenter to data json return without password

--- src/auth/users/users.controller.ts
+++ src/auth/users/users.controller.ts
@@ -1,13 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
+import { UserPresenter } from './use.presenter';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post()
-  create(@Body() data: CreateUserDto) {
-    return this.usersService.create(data);
+  async create(@Body() data: CreateUserDto) {
+    const user = await this.usersService.create(data);
+    return new UserPresenter(user)
   }
 }
