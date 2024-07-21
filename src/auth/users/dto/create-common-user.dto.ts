a7af9c4cb0d68cca8dbc0906959261353e28197e
aryelzx <arielcearamor102@gmail.com>
2024-07-21 10:44:51 -0300

    refactor: parse controller to common user

---
+++ src/auth/users/dto/create-common-user.dto.ts
@@ -0,0 +1,7 @@
+//dto - data transfer object
+
+export class CreateCommonUserDto {
+    name: string;
+    email: string;
+    password: string;
+}
--- src/auth/users/users.controller.ts
+++ src/auth/users/users.controller.ts
@@ -1,15 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreatePartnerUserDto } from './dto/create-partner-user.dto';
 import { UsersService } from './users.service';
 import { UserPresenter } from './use.presenter';
+import { CreateCommonUserDto } from './dto/create-common-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post()
-  async create(@Body() data: CreateUserDto) {
-    const user = await this.usersService.create(data);
+  async create(@Body() data: CreateCommonUserDto) {
+    const user = await this.usersService.createCommonUser(data);
     return new UserPresenter(user)
   }
 }
