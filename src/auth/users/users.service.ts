56a6afba5c8d3c4815569864dc3b0491639313ff
aryelzx <arielcearamor102@gmail.com>
2024-07-21 10:16:33 -0300

    feat: encrypt password with bcrypt

--- src/auth/users/users.service.ts
+++ src/auth/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserRoles } from './user-roles';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class UsersService {
@@ -9,12 +10,16 @@ export class UsersService {
     constructor(private prismaService: PrismaService) {}
 
     create(data: CreateUserDto) {
-        console.log(data, '***')
         return this.prismaService.user.create({
             data: {
                 ...data,
+                password: this.generateHash(data.password),
                 roles: [UserRoles.PARTNER],
             },
         });
+    }
+
+    generateHash(pass: string) {
+        return bcrypt.hashSync(pass, 10);
     }
 }
